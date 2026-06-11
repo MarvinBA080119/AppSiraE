4f8482a287e803f0650e9c3b1c70f6e43a3d7032
Marvin Betancourt <marvinbetancourt41@gmail.com>
2026-06-11 16:25:47 -0600

    Se arreglaron los fallos de la app para la 2 entrega del proyecto individual

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,8 @@ dependencies {
     implementation(libs.firebase.firestore)
     implementation(libs.coroutines.core)
     implementation(libs.coroutines.android)
+    implementation(libs.navigation.fragment.ktx)
+    implementation(libs.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
